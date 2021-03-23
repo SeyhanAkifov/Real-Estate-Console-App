d3e23ebeb3cd5974a433993882e9226790668496
Seyhan Akifov <55700582+SeyhanAkifov@users.noreply.github.com>
2021-03-23 14:26:22 +0100

    Added Data folder

---
+++ RealEstates.Data/ApplicationDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using RealEstates.Models;
+using Property = RealEstates.Models.Property;
+
+namespace RealEstates.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+
+        public DbSet<Property> Properties { get; set; }
+
+        public DbSet<BuildingType> BuildingTypes { get; set; }
+
+        public DbSet<PropertyType> PropertyTypes { get; set; }
+
+        public DbSet<District> Districts { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=RealEstates;Integrated Security=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
